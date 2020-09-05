d055de6746c8afdf6f36e1edb8b6f61cca2445ae
PankajSharma-1998 <pankaj00.ps79@gmail.com>
2020-09-05 17:20:41 +0530

    Updates

---
+++ precache-manifest.a31babf0f009d952f16969a158abf45d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "224f87c339ea5826399378943e901921",
+    "url": "/IMDB-clone/index.html"
+  },
+  {
+    "revision": "ad06fe5d8050fad09df6",
+    "url": "/IMDB-clone/static/css/main.e05085f0.chunk.css"
+  },
+  {
+    "revision": "bc49059f96301d6e0f58",
+    "url": "/IMDB-clone/static/js/2.fcc13881.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/IMDB-clone/static/js/2.fcc13881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad06fe5d8050fad09df6",
+    "url": "/IMDB-clone/static/js/main.edcbc63e.chunk.js"
+  },
+  {
+    "revision": "706fc3e224e0fa96f595",
+    "url": "/IMDB-clone/static/js/runtime-main.709b291c.js"
+  },
+  {
+    "revision": "6ee9362afb8f04f51f131e3b7f891245",
+    "url": "/IMDB-clone/static/media/screenimg.6ee9362a.jpg"
+  }
+]);
